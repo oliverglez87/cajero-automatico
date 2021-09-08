dcc2cc400f74fee69e2a5858d069d959ca5adb70
Oliver Gonzalez Novelo <olivergonzaleznovelo@MacBook-Air-de-Oliver.local>
2021-09-07 22:09:16 -0500

    Actualizacion funcion Depositar

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 let btnLogin = document.getElementById("btn-login");
 let seccionLogin = document.getElementById("seccionLogin");
 let seccionPrincipal = document.getElementById("seccionPrincipal");
+let formulario = document.getElementById("formulario");
+let mensajeError = document.getElementById("mensaje-error");
+let seccionSaldo = document.getElementById("seccionSaldo");
+let seccionIngresar = document.getElementById("seccionIngresar");
+let alertaNuevoSaldo = document.getElementById("alertaNuevoSaldo");
+
 
 seccionPrincipal.hidden = true;
+seccionSaldo.hidden =  true;
+seccionIngresar.hidden = true;
+alertaNuevoSaldo.hidden = true;
 
 var usuarios = [
     {
         name: "oliver",
         usuario: "oliver@gmail.com",
-        password: "12345",
+        password: "123",
         saldo: 500,   
     },
 
@@ -27,15 +36,61 @@ var usuarios = [
     }
 ]
 
+let usuarioActual;
 function login() {
-    var usuario = document.getElementById("exampleInputEmail1").value;
-    var password = document.getElementById("exampleInputPassword1").value;
 
-    if(usuario == usuarios[usuario] && password == usuarios[password]){
-        alert("Usuario y Password correctos");
-    }else{
-        alert("Verifique sus datos");
+let flagAlert = true;
+
+const correo = formulario.email.value;
+const password = formulario.password.value;
+
+if (correo.length <= 0 || password <= 0){
+    mensajeError.innerText= "No has ingresado tu Usuario y/o Contraseña";
+    return;
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+    let tmpUsuario = usuarios[i];
+    //Esta es la validacion para ver si si existe el usuario
+    if (
+    correo.toLowerCase() === tmpUsuario.usuario.toLowerCase() &&
+    password === tmpUsuario.password
+    ) {
+    flagAlert = false;
+    
+      //Oculto la sección de login (el formulario)
+    seccionLogin.hidden = true;
+      //Muestro la sección principal (los botones)
+    seccionPrincipal.hidden = false;
+    usuarioActual = tmpUsuario;
+    console.log(usuarioActual);
+    document.getElementById("username").innerText=usuarioActual.name;
+    break;
+    }else {
+        mensajeError.innerText="Usuario y/o Contraseña son incorrectos"
     }
-  }
+}
+}
 
-  login();
+function consultarSaldo() {
+    seccionSaldo.hidden = false;
+    seccionSaldo.innerText = "El saldo actual de tu cuenta es: " + usuarioActual.saldo;
+}
+
+function ingresarMonto() {
+    seccionIngresar.hidden = false;
+    
+}
+
+
+function depositar() {
+    let deposito1 = parseInt(seccionIngresar.deposito.value);
+    let nuevoSaldo = (usuarioActual.saldo+deposito1);
+
+    seccionSaldo.hidden = true;
+    alertaNuevoSaldo.hidden = false;
+
+    alertaNuevoSaldo.innerText = "Tu nuevo saldo es:" + nuevoSaldo;
+
+    // Para modificar el saldo en el arreglo??? --> usuarioActual.splice(3,1, nuevoSaldo);
+}
